b72bb8c639091ffbdf442ba10c97f4422d86bc7e
Konrad Opach <opach16@outlook.com>
2025-01-06 23:52:16 +0100

    first commit

---
+++ src/main/java/com/konrad/smartFinance/SmartFinanceViewApplication.java
@@ -0,0 +1,12 @@
+package com.konrad.smartFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFinanceViewApplication.class, args);
+    }
+}
